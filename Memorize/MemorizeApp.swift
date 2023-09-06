35526d05b2c18281699e7760ce660eb749b460ef
anamepisashvili <ana.mepisashvili@gmail.com>
2023-09-06 18:59:27 +0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ana Mepisashvili on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
